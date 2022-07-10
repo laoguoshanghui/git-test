fa994a81d5183e2e9ab557c5c8f1e20869fe45d7
ghj <ghj@qq.com>
2022-07-10 17:41:53 +0800

    first commit

---
+++ src/main/java/com/ghj/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.ghj.git;
+
+/**
+ * @author guojing
+ * @Date:2022/7/10 17:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
