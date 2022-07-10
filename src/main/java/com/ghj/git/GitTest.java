ae94ef91b73bf24a831ca75ded3fe4842002e226
ghj <ghj@qq.com>
2022-07-10 17:57:47 +0800

    third commit

--- src/main/java/com/ghj/git/GitTest.java
+++ src/main/java/com/ghj/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("second commit hello git!");
+        System.out.println("hot-fix commit hello git!");
     }
 }
